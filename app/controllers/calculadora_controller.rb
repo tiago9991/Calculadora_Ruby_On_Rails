a9d2da92d7f086bd9794b4d6a192f53c2bd5a539
Tiago <tiago.silva991@outlook.com>
2023-12-10 13:16:45 -0300

    Primeiro commit

---
+++ app/controllers/calculadora_controller.rb
@@ -0,0 +1,34 @@
+# app/controllers/calculadora_controller.rb
+
+class CalculadoraController < ApplicationController
+  def index
+  end
+
+  def calcular
+    operando1 = params[:operando1].to_f
+    operando2 = params[:operando2].to_f
+    operador = params[:operador].to_s.strip.downcase  # Remover espaços e converter para minúsculas
+
+    puts "Operador recebido: #{operador}"  # Saída de depuração
+
+    case operador
+    when '+'
+      @resultado = operando1 + operando2
+    when '-'
+      @resultado = operando1 - operando2
+    when '*'
+      @resultado = operando1 * operando2
+    when '/'
+      if operando2.zero?
+        @resultado = "Divisão por zero não permitida"
+      else
+        @resultado = operando1 / operando2
+      end
+    else
+      @resultado = "Operador inválido"
+    end
+
+    # Renderiza o resultado diretamente, mesmo que seja inválido
+    render json: { resultado: @resultado }
+  end
+end
